6f543dced1ae9e0c074426119b34a43315e1f1a8
Kevin Blumenfeld <github@thenext.net>
2019-07-21 12:45:57 -0400

    add missing function in perm bl script

--- Posh365Demo/BackLevel/Permissions.ps1
+++ Posh365Demo/BackLevel/Permissions.ps1
@@ -369,6 +369,29 @@ function Get-ADHashDisplayName {
     }
 }
 
+Function ConvertTo-NetBios {
+
+    Param(
+        $domainName
+    )
+
+    $root = [adsi] "LDAP://$domainname/RootDSE"
+    $configContext = $root.Properties["configurationNamingContext"][0]
+    $searchr = [adsi] "LDAP://cn=Partitions,$configContext"
+
+    $search = new-object System.DirectoryServices.DirectorySearcher
+    $search.SearchRoot = $searchr
+    $search.SearchScope = [System.DirectoryServices.SearchScope] "OneLevel"
+    $search.filter = "(&(objectcategory=Crossref)(dnsRoot=$domainName)(netBIOSName=*))"
+
+    $result = $search.Findone()
+
+    if ($result) {
+
+        $result.Properties["netbiosname"]
+    }
+
+}
 
 function Connect-Exchange {
     [CmdletBinding()]
