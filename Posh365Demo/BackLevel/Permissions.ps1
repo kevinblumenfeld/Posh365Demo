a08756915b5c91e4ea046a4b04f9e40d42094793
Kevin Blumenfeld <github@thenext.net>
2019-07-21 12:32:59 -0400

    Permissions ver 1

---
+++ Posh365Demo/BackLevel/Permissions.ps1
@@ -0,0 +1,441 @@
+
+if (-not (Get-Module ActiveDirectory -listavailable)) {
+    Write-Host "Please run from a computer with AD module" -ForegroundColor Red
+    break
+}
+$Exec = Get-ExecutionPolicy
+if ($Exec -eq 'Restricted') {
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Confirm:$false -ErrorAction SilentlyContinue
+}
+
+function Get-MailboxMoveOnPremisesPermissionReport {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ReportPath,
+
+        [Parameter()]
+        [switch]
+        $SkipSendAs,
+
+        [Parameter()]
+        [switch]
+        $SkipSendOnBehalf,
+
+        [Parameter()]
+        [switch]
+        $SkipFullAccess,
+
+        [Parameter()]
+        [switch]
+        $SkipFolderPerms
+    )
+    end {
+        New-Item -ItemType Directory -Path $ReportPath -ErrorAction SilentlyContinue
+
+        Write-Verbose "Caching hashtable. msExchRecipientTypeDetails numerical value as Key and Value of human readable"
+        $ADHashType = Get-ADHashType
+
+        Write-Verbose "Caching hashtable. msExchRecipientDisplayType numerical value as Key and Value of human readable"
+        $ADHashDisplay = Get-ADHashDisplay
+
+        $DelegateSplat = @{
+            SkipFullAccess   = $SkipFullAccess
+            SkipSendOnBehalf = $SkipSendOnBehalf
+            SkipSendAs       = $SkipSendAs
+            ADHashType       = $ADHashType
+            ADHashDisplay    = $ADHashDisplay
+            ErrorAction      = 'SilentlyContinue'
+        }
+        if ($DelegateSplat.Values -contains $false) {
+            try {
+                Import-Module ActiveDirectory -ErrorAction Stop -Verbose:$false
+            }
+            catch {
+                Write-Host "This module depends on the ActiveDirectory module."
+                Write-Host "Please download and install from https://www.microsoft.com/en-us/download/details.aspx?id=45520"
+                Write-Host "or run Connect-Exchange from a server with the Active Directory Module installed"
+                throw
+            }
+        }
+        $DomainNameHash = Get-DomainNameHash
+        Write-Verbose "Importing Active Directory Users and Groups that have at least one proxy address"
+
+        $ADUserList = Get-ADUsersandGroupsWithProxyAddress -DomainNameHash $DomainNameHash
+        Write-Verbose "Retrieving all Exchange Mailboxes"
+        $MailboxList = Get-Mailbox -ResultSize unlimited
+        if ($DelegateSplat.Values -contains $false) {
+            $DelegateSplat.Add('MailboxList', $MailboxList)
+            $DelegateSplat.Add('ADUserList', $ADUserList)
+            Write-Verbose "Mailbox`t$($Mailbox.DisplayName)"
+            Get-MailboxMoveMailboxPermission @DelegateSplat | Export-Csv (Join-Path $ReportPath 'MailboxPermissions.csv') -NoTypeInformation -Encoding UTF8
+            $MailboxFile = Join-Path $ReportPath 'MailboxPermissions.csv'
+        }
+        if (-not $SkipFolderPerms) {
+            $FolderPermSplat = @{
+                MailboxList   = $MailboxList
+                ADUserList    = $ADUserList
+                ADHashType    = $ADHashType
+                ADHashDisplay = $ADHashDisplay
+                ErrorAction   = 'SilentlyContinue'
+            }
+            Get-MailboxMoveFolderPermission @FolderPermSplat | Export-Csv (Join-Path $ReportPath 'FolderPermissions.csv') -NoTypeInformation -Encoding UTF8
+            $FolderFile = Join-Path $ReportPath 'FolderPermissions.csv'
+        }
+        $ExcelSplat = @{
+            Path                    = (Join-Path $ReportPath 'Permissions.xlsx')
+            TableStyle              = 'Medium2'
+            FreezeTopRowFirstColumn = $true
+            AutoSize                = $true
+            BoldTopRow              = $true
+            ClearSheet              = $true
+            ErrorAction             = 'SilentlyContinue'
+        }
+        $MailboxFile, $FolderFile | Where-Object { $_ } | ForEach-Object { Import-Csv $_ | Export-Excel @ExcelSplat -WorksheetName ($_ -replace '.+\\|permissions\.csv') }
+    }
+}
+
+function Get-ADHashType {
+    param (
+
+    )
+    end {
+        $TypeDetails = @{
+            '1'            = 'UserMailbox'
+            '2'            = 'LinkedMailbox'
+            '4'            = 'SharedMailbox'
+            '8'            = 'LegacyMailbox'
+            '16'           = 'RoomMailbox'
+            '32'           = 'EquipmentMailbox'
+            '64'           = 'MailContact'
+            '128'          = 'MailEnabledUser'
+            '256'          = 'MailEnabledUniversalDistributionGroup'
+            '512'          = 'MailEnabledNonUniversalDistributionGroup'
+            '1024'         = 'MailEnabledUniversalSecurityGroup'
+            '2048'         = 'DynamicDistributionGroup'
+            '4096'         = 'MailEnabledPublicFolder'
+            '8192'         = 'SystemAttendantMailbox'
+            '16384'        = 'MailboxDatabaseMailbox'
+            '32768'        = 'AcrossForestMailContact'
+            '65536'        = 'User'
+            '131072'       = 'Contact'
+            '262144'       = 'UniversalDistributionGroup'
+            '524288'       = 'UniversalSecurityGroup'
+            '1048576'      = 'Non-UniversalGroup'
+            '2097152'      = 'DisabledUser'
+            '4194304'      = 'MicrosoftExchange'
+            '8388608'      = 'ArbitrationMailbox'
+            '16777216'     = 'MailboxPlan'
+            '33554432'     = 'LinkedUser'
+            '268435456'    = 'RoomList'
+            '536870912'    = 'DiscoverMailbox'
+            '1073741824'   = 'RoleGroup'
+            '2147483648'   = 'RemoteUserMailbox'
+            '8589934592'   = 'RemoteRoomMailbox'
+            '17173869184'  = 'RemoteEquipmentMailbox'
+            '34359738368'  = 'RemoteSharedMailbox'
+            '137438953472' = 'TeamMailbox'
+        }
+        $TypeDetails
+    }
+}
+
+function Get-ADHashDisplay {
+    param (
+
+    )
+    end {
+        $Display = @{
+            '0'           = 'MailboxUser'
+            '1'           = 'DistributionGroup'
+            '2'           = 'PublicFolder'
+            '3'           = 'DynamicDistributionGroup'
+            '4'           = 'Organization'
+            '5'           = 'PrivateDistributionList'
+            '6'           = 'RemoteMailUser'
+            '7'           = 'ConferenceRoomMailbox'
+            '8'           = 'EquipmentMailbox'
+            '10'          = 'ArbitrationMailbox'
+            '11'          = 'MailboxPlan'
+            '12'          = 'LinkedUser'
+            '15'          = 'RoomList'
+            '1073741833'  = 'SecurityDistributionGroup'
+            '1073741824'  = 'ACLableMailboxUser'
+            '1073741830'  = 'ACLableRemoteMailUser'
+            '-2147481343' = 'SyncedUSGasUDG'
+            '-1073739511' = 'SyncedUSGasUSG'
+            '-2147481338' = 'SyncedUSGasContact'
+            '-1073739514' = 'ACLableSyncedUSGasContact'
+            '-2147482874' = 'SyncedDynamicDistributionGroup'
+            '-1073741818' = 'ACLableSyncedMailboxUser'
+            '-2147483642' = 'SyncedMailboxUser'
+            '-2147481850' = 'SyncedConferenceRoomMailbox'
+            '-2147481594' = 'SyncedEquipmentMailbox'
+            '-2147482106' = 'SyncedRemoteMailUser'
+            '-1073740282' = 'ACLableSyncedRemoteMailUser'
+            '-2147483130' = 'SyncedPublicFolder'
+
+        }
+        $Display
+    }
+}
+
+function Get-DomainNameHash {
+
+    param (
+
+    )
+    end {
+        $DomainNameHash = @{ }
+
+        $DomainList = ([System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().domains) | Select -ExpandProperty Name
+        foreach ($Domain in $DomainList) {
+            $DomainNameHash[$Domain] = (ConvertTo-NetBios -domain $Domain)
+        }
+        $DomainNameHash
+    }
+}
+
+function Get-ADUsersAndGroupsWithProxyAddress {
+    param (
+        [Parameter()]
+        [hashtable] $DomainNameHash
+    )
+    try {
+        import-module activedirectory -ErrorAction Stop -Verbose:$false
+    }
+    catch {
+        Write-Host "This module depends on the ActiveDirectory module."
+        Write-Host "Please download and install from https://www.microsoft.com/en-us/download/details.aspx?id=45520"
+        throw
+    }
+
+    # Find writable Global Catalog
+    $context = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('Forest')
+    $dc = ([System.DirectoryServices.ActiveDirectory.GlobalCatalog]::FindOne($context, [System.DirectoryServices.ActiveDirectory.LocatorOptions]'ForceRediscovery, WriteableRequired')).name
+
+    $Selectproperties = @(
+        'DisplayName', 'UserPrincipalName', 'distinguishedname', 'SamAccountName', 'ProxyAddresses'
+        'canonicalname', 'mail', 'Objectguid', 'msExchRecipientTypeDetails'
+        'msExchRecipientDisplayType'
+    )
+    $CalculatedProps = @(
+        @{n = "logon"; e = { ($DomainNameHash.($_.distinguishedname -replace '^.+?DC=' -replace ',DC=', '.')) + "\" + $_.samaccountname } },
+        @{n = "PrimarySMTPAddress" ; e = { ( $_.proxyAddresses | Where-Object { $_ -cmatch "SMTP:*" }).Substring(5) } }
+    )
+    Get-ADUser -filter 'proxyaddresses -ne "$null"' -server ($dc + ":3268") -SearchBase (Get-ADRootDSE).rootdomainnamingcontext -SearchScope Subtree -Properties $SelectProperties |
+    Select-Object ($Selectproperties + $CalculatedProps)
+    Get-ADGroup -filter 'proxyaddresses -ne "$null"' -server ($dc + ":3268") -SearchBase (Get-ADRootDSE).rootdomainnamingcontext -SearchScope Subtree -Properties $SelectProperties |
+    Select-Object ($Selectproperties + $CalculatedProps)
+}
+
+function Get-MailboxMoveMailboxPermission {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [switch]
+        $SkipSendAs,
+
+        [Parameter()]
+        [switch]
+        $SkipSendOnBehalf,
+
+        [Parameter()]
+        [switch]
+        $SkipFullAccess,
+
+        [Parameter(Mandatory = $true)]
+        $MailboxList,
+
+        [Parameter(Mandatory = $true)]
+        $ADUserList,
+
+        [parameter()]
+        [hashtable]
+        $ADHashType,
+
+        [parameter()]
+        [hashtable]
+        $ADHashDisplay
+    )
+    end {
+        Write-Verbose "Caching hashtable. LogonName as Key and Values of DisplayName & UPN"
+        $ADHash = $ADUserList | Get-ADHash
+
+        Write-Verbose "Caching hashtable. DN as Key and Values of DisplayName, UPN & LogonName"
+        $ADHashDN = $ADUserList | Get-ADHashDN
+
+        Write-Verbose "Caching hashtable. CN as Key and Values of DisplayName, UPN & LogonName"
+        $ADHashCN = $ADUserList | Get-ADHashCN
+
+        $MailboxDN = $MailboxList | Select-Object -expandproperty distinguishedname
+
+        $PermSelect = @(
+            'Object', 'UserPrincipalName', 'PrimarySMTPAddress', 'Granted', 'GrantedUPN'
+            'GrantedSMTP', 'Checking', 'TypeDetails', 'DisplayType', 'Permission'
+        )
+        $ParamSplat = @{
+            ADHashDN      = $ADHashDN
+            ADHash        = $ADHash
+            ADHashType    = $ADHashType
+            ADHashDisplay = $ADHashDisplay
+        }
+        $ParamSOBSplat = @{
+            ADHashCN      = $ADHashCN
+            ADHashDN      = $ADHashDN
+            ADHashType    = $ADHashType
+            ADHashDisplay = $ADHashDisplay
+        }
+        if (-not $SkipSendAs) {
+            Write-Verbose "Getting SendAs permissions for each mailbox and writing to file"
+            $MailboxDN | Get-SendAsPerms @ParamSplat |
+            Select-Object $PermSelect
+        }
+        if (-not $SkipSendOnBehalf) {
+            Write-Verbose "Getting SendOnBehalf permissions for each mailbox and writing to file"
+            ($MailboxList | Where-Object { $_.GrantSendOnBehalfTo }) | Get-SendOnBehalfPerms @ParamSOBSplat |
+            Select-Object $PermSelect
+        }
+        if (-not $SkipFullAccess) {
+            Write-Verbose "Getting FullAccess permissions for each mailbox and writing to file"
+            $MailboxDN | Get-FullAccessPerms @ParamSplat |
+            Select-Object $PermSelect
+        }
+    }
+}
+
+function Get-MailboxMoveFolderPermission {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        $MailboxList,
+
+        [Parameter(Mandatory = $true)]
+        $ADUserList,
+
+        [parameter()]
+        [hashtable]
+        $ADHashType,
+
+        [parameter()]
+        [hashtable]
+        $ADHashDisplay
+    )
+    end {
+        $FolderSelect = @(
+            'Object', 'UserPrincipalName', 'PrimarySMTPAddress', 'Folder', 'AccessRights'
+            'Granted', 'GrantedUPN', 'GrantedSMTP', 'TypeDetails', 'DisplayType'
+        )
+        Write-Verbose "Caching hashtable. DisplayName as Key and Values of UPN, PrimarySMTP, msExchRecipientTypeDetails & msExchRecipientDisplayType"
+        $ADHashDisplayName = $ADUserList | Get-ADHashDisplayName -erroraction silentlycontinue
+
+        $FolderPermSplat = @{
+            ADHashDisplayName = $ADHashDisplayName
+            ADHashType        = $ADHashType
+            ADHashDisplay     = $ADHashDisplay
+        }
+        Write-Verbose "Getting Folder Permissions for each mailbox and writing to file"
+        $MailboxList | Get-MailboxFolderPerms @FolderPermSplat | Select-Object $FolderSelect
+    }
+}
+
+function Get-ADHashDisplayName {
+
+    param (
+        [parameter(ValueFromPipeline = $true)]
+        $ADUserList
+    )
+    begin {
+        $ADHashDisplayName = @{ }
+    }
+    process {
+        foreach ($ADUser in $ADUserList) {
+            $ADHashDisplayName[$ADUser.DisplayName] = @{
+                UserPrincipalName          = $ADUser.UserPrincipalName
+                PrimarySMTPAddress         = $ADUser.PrimarySMTPAddress
+                msExchRecipientTypeDetails = $ADUser.msExchRecipientTypeDetails
+                msExchRecipientDisplayType = $ADUser.msExchRecipientDisplayType
+            }
+        }
+    }
+    end {
+        $ADHashDisplayName
+    }
+}
+
+
+function Connect-Exchange {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [string]
+        $Server,
+
+        [Parameter()]
+        [Switch]
+        $DeleteExchangeCreds,
+
+        [Parameter()]
+        [Switch]
+        $DontViewEntireForest
+    )
+
+    $CredFile = Join-Path $Env:USERPROFILE ConnectExchange.xml
+    if ($DeleteExchangeCreds) { Remove-Item $CredFile -Force }
+
+    if (-not ($null = Test-Path $CredFile)) {
+        [System.Management.Automation.PSCredential]$Credential = Get-Credential -Message 'Enter on-premises Exchange username and password'
+        [System.Management.Automation.PSCredential]$Credential | Export-CliXml -Path $CredFile
+        [System.Management.Automation.PSCredential]$Credential = Import-CliXml -Path $CredFile
+    }
+    else {
+        [System.Management.Automation.PSCredential]$Credential = Import-CliXml -Path $CredFile
+    }
+    $SessionSplat = @{
+        Name              = "OnPremExchange"
+        ConfigurationName = 'Microsoft.Exchange'
+        ConnectionUri     = ("http://" + $Server + "/PowerShell/")
+        Authentication    = 'Kerberos'
+        Credential        = $Credential
+    }
+    $Session = New-PSSession @SessionSplat
+    $SessionModule = Import-PSSession -AllowClobber -DisableNameChecking -Session $Session
+    $null = Import-Module $SessionModule -Global -DisableNameChecking -Force
+    if (-not $DontViewEntireForest) {
+        Set-ADServerSettings -ViewEntireForest:$True
+    }
+    Write-Host "Connected to Exchange Server: $Server" -ForegroundColor Green
+}
+
+
+$InstallSplat = @{
+    Name        = 'ImportExcel'
+    Scope       = 'CurrentUser'
+    Force       = $true
+    ErrorAction = 'SilentlyContinue'
+    Confirm     = $false
+}
+
+Install-Module @InstallSplat
+
+function Get-Answer {
+    $Answer = Read-Host "Connect to Exchange Server? (Y/N)"
+    if ($Answer -ne "Y" -and $Answer -ne "N") {
+        Get-Answer
+    }
+    if ($Answer -eq "Y") {
+        $ServerName = Read-Host "Type then name of the Exchange Server and hit enter"
+        Connect-Exchange -Server $ServerName
+    }
+
+}
+Get-Answer
+Get-MailboxMoveOnPremisesPermissionReport -ReportPath ([Environment]::GetFolderPath("Desktop")) -Verbose
+
+
+
+
+
+#### END  ###
