d578a84c056450fa1c0c1a4e409618ac29d39f98
Kevin Blumenfeld <github@thenext.net>
2019-07-21 12:13:13 -0400

    Corrected message to onprem for pscred

--- Posh365Demo/6. OnPremises.ps1
+++ Posh365Demo/6. OnPremises.ps1
@@ -1,5 +1,5 @@
 
-# Create Batches.xlsx
+# Create Batches.xlsx - copy code from http://bit.ly/corebatches and paste on any server with AD
 Get-MailboxMoveOnPremisesMailboxReport
 
 # Create Permissions.xlsx
