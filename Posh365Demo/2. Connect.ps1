f3d944ceee14646467f54661eb602c627e0b7177
Kevin Blumenfeld <github@thenext.net>
2019-07-22 10:09:13 -0400

    Connect demo updates

--- Posh365Demo/2. Connect.ps1
+++ Posh365Demo/2. Connect.ps1
@@ -1,3 +1,12 @@
 
-# Connect to Exchange Online & AzureAD
-Connect-CloudMFA -Tenant Contoso -ExchangeOnline -AzureAD
+# Connect to Exchange Online
+Connect-CloudMFA -Tenant Contoso -ExchangeOnline
+
+# Connect to Microsoft Online and AzureAD
+Connect-CloudMFA -Tenant Contoso -MSOnline -AzureAD
+
+# Connect to Security and Compliance
+Connect-CloudMFA -Tenant Contoso -Compliance
+
+# Connect to SharePoint
+Connect-CloudMFA -Tenant Contoso -SharePoint
