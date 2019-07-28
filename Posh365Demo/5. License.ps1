e0b86ba3710e3e4f1dbbf9441bd74c5b82242696
Kevin Blumenfeld <github@thenext.net>
2019-07-28 14:40:13 -0400

    Add csv option

--- Posh365Demo/5. License.ps1
+++ Posh365Demo/5. License.ps1
@@ -4,23 +4,15 @@ Connect-CloudMFA mkevin -AzureAD # minimum for licensing cmdlets
 
 # Set Licenses
 Set-MailboxMoveLicense -SharePointURL 'https://CoreBTStest.sharepoint.com/sites/mkevin' -ExcelFile 'Batches.xlsx'
-Set-MailboxMoveLicense -MailboxCSV 'C:\scripts\Batches2.csv' # to use a CSV instead of Excel
+
+# To use CSV instead
+Set-MailboxMoveLicense -MailboxCSV 'C:\scripts\Batches.csv'
 
 # Get License Report for user(s)
 Get-MailboxMoveLicense -SharePointURL 'https://CoreBTStest.sharepoint.com/sites/mkevin' -ExcelFile 'Batches.xlsx'
-Get-MailboxMoveLicense -MailboxCSV 'C:\scripts\Batches2.csv' # to use a CSV instead of Excel
+
+# To use CSV instead
+Get-MailboxMoveLicense -MailboxCSV 'C:\scripts\Batches.csv' # to use a CSV instead of Excel
 
 # Tenant License Count
 Get-MailboxMoveLicenseCount
-
-$ExcelSplat = @{
-    Path                    = 'c:\scripts\LicExcelTest.xlsx'
-    FreezeTopRowFirstColumn = $true
-    AutoSize                = $true
-    ClearSheet              = $true
-}
-Import-Csv C:\Scripts\356_Licenses.csv | Export-Excel @ExcelSplat -ConditionalText $(
-    New-ConditionalText DisplayName White DarkBlue
-    New-ConditionalText UserPrincipalName White DarkBlue
-    New-ConditionalText AccountSku White DarkBlue
-)
