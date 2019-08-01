c0df6bfce63bf64d9b743c5d78d72b5ac7064b16
Kevin Blumenfeld <github@thenext.net>
2019-08-01 13:43:26 -0400

    upgrade comment

--- Posh365Demo/1. Install.ps1
+++ Posh365Demo/1. Install.ps1
@@ -2,10 +2,11 @@
 # Set Execution Policy (one-time task)
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
-# Install Posh365. Requires Win10 or Win2016. Type "Y" when prompted.
+# Install Posh365. Requires Win10 or Win2016. Type "Y" when prompted. Restart PowerShell if you upgrading Posh365.
 Install-Module Posh365 -Force -Scope CurrentUser
 
 # Remotely install Posh365 on older OS. Requires access to remote computer's C$
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 Install-Module Posh365 -Force -Scope CurrentUser
 Install-ModuleOnServer -Server DC01  # modify server name
+
